fd779abebc49f17099a0b479348e88dba8a704e9
jonatiao <jonatiao@gmail.com>
2018-08-22 16:56:53 -0400

    correcao do html pra compilar no travis

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Minha pagino do codeanywhere lol.")
+        self.assertRegex(result.data.decode(), "Minha pagina do codeanywhere lol")
 
 
 if __name__ == "__main__":
